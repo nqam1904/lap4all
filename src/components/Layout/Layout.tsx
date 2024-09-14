66da711f37ea50c78a0c7e0ab63f223ed5ec833b
minh.nghiem <minh.nghiem@mservice.com.vn>
2024-09-14 21:19:49 +0700

    cm

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import styles from "./layout.module.scss";
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.main}>
+          {/* {!noCategories && <CategoriesBar />} */}
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Layout;
